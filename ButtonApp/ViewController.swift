bd4723962d498a5d801faf464b287d261e1e4d88
Ivan Zakharov <vanio16@yandex.ru>
2020-12-07 01:09:12 +0600

    Hi GIT

--- ButtonApp/ViewController.swift
+++ ButtonApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    
+    let newText = "New text"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeText(_ sender: Any) {
+        textLabel.text = newText
+    }
+    
 }
 
