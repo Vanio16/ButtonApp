f45c520cb6f2f8c3f12d5b134f8179e00add274e
Ivan Zakharov <vanio16@yandex.ru>
2020-12-09 15:40:24 +0600

    added new images to project, changed background of the project to picture, added textField, added button to change background

--- ButtonApp/ViewController.swift
+++ ButtonApp/ViewController.swift
@@ -9,17 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
-    
-    let newText = "New text"
+    @IBOutlet weak var contentTextField: UITextField!
+    @IBOutlet weak var backgroundImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func changeText(_ sender: Any) {
-        textLabel.text = newText
-    }
+    var numberBackImage = 0
     
+    
+    @IBAction func changeText(_ sender: Any) {
+        textLabel.text = contentTextField.text
+        view.endEditing(true)
+    }
+    @IBAction func changeBackground(_ sender: Any) {
+        numberBackImage += 1
+        if numberBackImage == 3 {
+            numberBackImage = 0
+        }
+        switch numberBackImage {
+            case 0: backgroundImage.image = UIImage(named: "first")
+            case 1: backgroundImage.image = UIImage(named: "second")
+            case 2: backgroundImage.image = UIImage(named: "third")
+            default: numberBackImage = 0
+        }
+        
+    }
+
 }
 
